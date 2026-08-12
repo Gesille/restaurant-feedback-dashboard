a1892b61690aecc1bb95b6257c6264a0ff81ca02
Giselle Georges <giselle@nextintl.com>
2026-08-12 14:57:07 -0400

    employeeAnalytics

--- redux/Employee/Employeeapi.ts
+++ redux/Employee/Employeeapi.ts
@@ -341,7 +341,7 @@ export const employeeApi = apiSlice.injectEndpoints({
       providesTags: [{ type: "Employee", id: "CONTRACTS_NEAR_END" }],
     }),
     getEmployeeAnalytics: builder.query<EmployeeAnalytics, void>({
-      query: () => "/employees/analytics-employee",
+      query: () => "/employees/analytics",
       transformResponse: (res: { data: EmployeeAnalytics }) => res.data,
     }),
     updatePotentialBonus: builder.mutation<
