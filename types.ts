bcd0f337543f9cde9d730930962aee9333ad8a42
Giselle Georges <giselle@nextintl.com>
2026-08-12 10:23:56 -0400

    employee

--- types.ts
+++ types.ts
@@ -333,6 +333,7 @@ export interface EmployeeJobTab {
     job_code?: string;
     direct_reports_count: number;
     probation_end_date?: string;
+    probation_pending?: boolean;
     contract_end_date?: string;
     contracted_hours_per_week?: number;
     contracted_days_per_week?: number;
